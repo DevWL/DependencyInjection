6536da3f0205e288f175658bcafa49901fda90c4
GlupiJas <w.liszkiewicz@gmail.com>
2016-10-02 04:00:36 +0200

    phpdoc comments

--- DependencyInjector.php
+++ DependencyInjector.php
@@ -1,10 +1,22 @@
 <?php 
 
+/**
+ * Holds an array of services/closures and data
+ * Provides magic method interface to set and retrive services/data
+ * Services should be stored in closures
+ * Function getService return lambada
+ */
 class DependencyInjector
 {
 
+    /**
+     * @var holds an array of services and other values
+     */
     private $services = [];
 
+    /**
+     * Create new instance
+     */
     public function __construct(){
     }
 
@@ -17,10 +29,19 @@ class DependencyInjector
         }
     }
 
+    /**
+     * Get service
+     * @param service name
+     * @param mix closure/service or variable
+     */
     public function setService($name, $service){
         $this->services[$name] = $service;
     }
 
+    /**
+     * Get service
+     * @param service name
+     */
     public function getService($name){
         try{
             if(array_key_exists($name, $this->services)){
@@ -32,6 +53,13 @@ class DependencyInjector
         }
     }
 
+    /**
+     * Call function directly from a container. 
+     * !important function has to take arguments in an array or none
+     * us of this method require specyfic build of a closure
+     * @param service name
+     * @param arrey of arguments for that service
+     */
     public function runService($name, Array $args = []){
         try{
             if(is_callable($this->services[$name])){
@@ -53,16 +81,29 @@ class DependencyInjector
         }
     }
 
+    /**
+     * Get service directly calling on container method
+     * @param service name $container->name()
+     */
     public function __get($name){
         return $this->getService($name);
     }
 
+    /**
+     * Set service or variable
+     * @param service name
+     * @param closure, anonymus function or variable
+     */
     public function __set($name, $value){
         $this->setService($name, $value);
     }
 
+    /**
+     * Prints class name
+     * @param
+     */
     public function __toString(){
-        return "DependencyInjector<br>".PHP_EOL;
+        return "DependencyInjector ".PHP_EOL;
     }
 
 }
